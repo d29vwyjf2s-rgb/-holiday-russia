81f915b12e3509355b6f24d1cc306914a6057967
d29vwyjf2s-rgb <d29vwyjf2s@privaterelay.appleid.com>
2026-09-19 00:11:51 +0300

    Delete app/SRC/main/java/EU directory

--- app/SRC/main/java/EU/rusholiday/pro/Holiday.java
+++
@@ -1,5 +0,0 @@
-package ru.rusholiday.pro;
-public class Holiday {
-    public final String date, title, category;
-    public Holiday(String date, String title, String category){this.date=date;this.title=title;this.category=category;}
-}
