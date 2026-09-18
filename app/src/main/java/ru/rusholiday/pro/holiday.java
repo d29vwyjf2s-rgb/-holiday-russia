ac4051f09c5c175f2412f8652a9b8b5ec371c2bb
d29vwyjf2s-rgb <d29vwyjf2s@privaterelay.appleid.com>
2026-09-19 00:15:40 +0300

    Create holiday.java

---
+++ app/src/main/java/ru/rusholiday/pro/holiday.java
@@ -0,0 +1,5 @@
+package ru.rusholiday.pro;
+public class Holiday {
+    public final String date, title, category;
+    public Holiday(String date, String title, String category){this.date=date;this.title=title;this.category=category;}
+}
